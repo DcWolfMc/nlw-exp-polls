60835868b9b8bd43c4f68735c976eb5f276dca92
Daniel Colares <danielmcolares@gmail.com>
2024-02-10 19:34:33 -0300

    more routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,22 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { VoteOnPoll } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie"
+
 
 const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(cookie, {
+  secret: "poll-app-nlw-ddd",
+  hook: 'onRequest',
 
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
+})
 
-  const { title } = createPollBody.parse(request.body);
+app.register(createPoll)
+app.register(getPoll)
+app.register(VoteOnPoll)
 
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  })
-
-  return reply.status(201).send(poll);
-});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
